bacade03a06e155231a590fa84b3dca3e97b8972
Elmir94 <34394087+Elmir94@users.noreply.github.com>
2019-04-30 12:57:52 +0200

    Multiple sessions implemented
    Can handle multiple sessions through allowing for connection to different ports

--- Program.cs
+++ Program.cs
@@ -4,8 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
-using System.Web.WebSockets;
-using System.Net.WebSockets;
+
 using SuperWebSocket;
 using System.Linq;
 
@@ -16,59 +15,87 @@ namespace ServerCS
         static int m_DebugImageNr = 0;
         private static WebSocketServer m_wsServer;
         private static string[] m_ImageList = new string[100000];
+        private static string[] m_ImageListOutputTest = new string[100000];
+        
         private static string[] m_ClientFeedList = new string[20];
         private static int m_NrOfClientsConnected = 0;
+        private static WebSocketSession[] m_WsSessions = new WebSocketSession[10];
+
         static void Main(string[] args)
         {
+            //initialise array that will be handling the image data
             for(int i =0;i<100000;i++)
             {
                 m_ImageList[i] = "";
+                m_ImageListOutputTest[i] = "";
             }
             m_wsServer = new WebSocketServer();
-            m_wsServer.Setup(3000);
+
+            var m_ServerConfig = new SuperSocket.SocketBase.Config.ServerConfig();
+            m_ServerConfig.MaxConnectionNumber = 100;
+            var m_List = new List<SuperSocket.SocketBase.Config.ListenerConfig>(10);
+            int port = 3000;
+            //Initialise ports, these will be used to handle different sessions
+            for (int i = 0; i < 10; i++)
+            {
+                var listener = new SuperSocket.SocketBase.Config.ListenerConfig();
+                listener.Port = port;
+                port++;
+                listener.Backlog = 1000;
+                listener.Ip = "Any";
+                listener.Security = "None";
+
+                m_List.Add(listener);
+            }
+            //Setup the server and subscribe the server to the various events
+            m_ServerConfig.Listeners = m_List;
+            m_wsServer.Setup(m_ServerConfig);
             m_wsServer.NewSessionConnected += WsServer_NewSessionConnected;
             m_wsServer.NewMessageReceived += WsServer_NewMessageReceived;
             m_wsServer.NewDataReceived += WsServer_NewDataReceived;
             m_wsServer.SessionClosed += WsServer_SessionClosed;
             m_wsServer.Start();
-           
             Console.ReadKey();
           
         }
 
         
-        private static void PackageBytesAndSend(WebSocketSession _Session,string _Data)
+        private static void PackageBytesAndSend(WebSocketSession _Session,string _Data,int _sessionId)
         {
             string[] _Payload = new string[_Data.Length/1000];
             _Payload = _Data.Split(',');
             foreach (string _SplitPayload in _Payload)
             {
-                _Session.Send(_SplitPayload);
+                m_WsSessions[_sessionId].Send(_SplitPayload);
             }
-           
         }
         private static void WsServer_NewSessionConnected(WebSocketSession _Session)
         {
-          //  m_ClientFeedList[m_NrOfClientsConnected] += _Session.RemoteEndPoint;
+           m_ClientFeedList[m_NrOfClientsConnected] += _Session.RemoteEndPoint;
             m_NrOfClientsConnected++;
+            m_WsSessions[1] = _Session;
+            Console.WriteLine("New user connected: " + _Session.RemoteEndPoint);
+
         }
         private static void WsServer_NewMessageReceived(WebSocketSession _Session, string _Value)
         {
-            string _imageNrString = _Value.Substring(1, 7);
+            string _sessionId = _Value.Substring(0,1);
+            int _sessionIdInt = Convert.ToInt32(_sessionId);
+            string _imageNrString = _Value.Substring(3, 7);
             string _RemoteClient = _Session.RemoteEndPoint.ToString();
             int _imageNrInt = Convert.ToInt32(_imageNrString);
-            string _imagevalue = _Value.Substring(8, _Value.Length - 8);
+            string _imagevalue = _Value.Substring(9, _Value.Length - 9);
             m_ImageList[_imageNrInt] += _RemoteClient+';'+_imagevalue+',';
+            m_ImageListOutputTest[_imageNrInt] += _imagevalue;
 
             if (_imagevalue.Length<1000)
             {
-                PackageBytesAndSend(_Session, m_ImageList[_imageNrInt]);
-                Console.WriteLine(m_ImageList[_imageNrInt]+"                    " +
+                PackageBytesAndSend(_Session, m_ImageList[_imageNrInt],_sessionIdInt);
+                Console.WriteLine(m_ImageList[_imageNrInt] + "                    " +
                     "                     " +
                     "DEBUG IMAGE NR" +
-                   m_DebugImageNr + "Client ip: "+_Session.RemoteEndPoint);
+                   m_DebugImageNr + "Client ip: " + _Session.RemoteEndPoint);
                 m_DebugImageNr++;
-
             }
 
         }
